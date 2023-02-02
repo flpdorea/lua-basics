a52b42340fe52ab5614ad4ad9e20e0daac37c593
flpdorea <felipeldorea@gmail.com>
2023-02-01 22:49:32 -0300

    add variables.lua

---
+++ variables.lua
@@ -0,0 +1,41 @@
+print("Variable examples: ")
+
+Score = 100
+print(Score)
+
+Salary = 40000.50
+print(Salary)
+
+Bank_balance = -10
+print(Bank_balance)
+
+local p1_name = "Leeroy Jenkins"
+local p2_name = "Felipe Dórea"
+print(#p2_name)
+print(p1_name.." and "..p2_name)
+-- .. is used to concatenate two strings
+
+print(type(Score))
+print(type(Salary))
+print(type(Bank_balance))
+print(type(p1_name))
+
+-- Data types:
+-- nil: used to differentiate the value from having some data or no(nil) data
+-- boolean: includes true and false as values
+-- number: real(double precision floating point) numbers
+-- string: array of characters
+-- function (CFuntion): a method that is written in C or Lua
+-- userdata: arbitrary C data
+-- thread: independent threads of execution and it is used to implement routines
+-- table: ordinary arrays, symbol tables, sets, records, graphs, trees, etc., and implements associative arrays; it can hold any value (except nil)
+
+print(10 + 10)
+print(10 - 10)
+print(10 * 10)
+print(10 / 10)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+print(10 ^ 10)
+print(-Bank_balance)
